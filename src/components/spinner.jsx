5b57b8b9357ad3ed02e8041b3cf69308496caa70
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-07-31 12:45:29 +0800

    update spinner

--- src/components/spinner.jsx
+++ src/components/spinner.jsx
@@ -2,7 +2,7 @@ import { ClipLoader } from "react-spinners/ClipLoader"
 
 const override = {
     display: 'block',
-    margin: '100px auto',
+    margin: '100px auto'
 }
 
 const spinner = ({loading}) => {
