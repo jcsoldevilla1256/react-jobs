2362878104ef27e4d202751a0a9662af01be2ae9
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-08-01 23:10:11 +0800

    Add edit job page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage';
 import JobPage, {jobLoader} from './pages/JobPage';
 import AddJobPage from './pages/AddJobPage';
-
+import EditJobPage from './pages/EditJobPage';
 
 
 const App = () => {
@@ -21,6 +21,18 @@ const App = () => {
     return;
   };
 
+  //update Job
+  const updateJob = async(job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application,json'
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  }
+
   //Delete Job
   const deleteJob = async (id) => {
     const res = await fetch(`/api/jobs/${id}`, {
@@ -35,6 +47,7 @@ const App = () => {
       <Route index element ={<HomePage/>} />
       <Route path='/jobs' element ={<JobsPage/>} />
       <Route path='/add-job' element ={<AddJobPage addJobSubmit={addJob}/>} />
+      <Route path='/edit-job/:id' element ={<EditJobPage updateJobSubmit={updateJob}/>} loader={jobLoader}/>
       <Route path='/jobs/:id' element ={<JobPage deleteJob = {deleteJob}/>} loader={jobLoader}/>
       <Route path='*' element ={<NotFoundPage/>} />
     </Route>
